1873a3c9bc52690a4b3284c9b293a7bf3cadec14
FloydWatson <floydbcseit@gmail.com>
2020-03-16 21:10:06 +1300

    added listing screen. passing id as arguments

--- fisbomock/lib/Widgets/listing_item.dart
+++ fisbomock/lib/Widgets/listing_item.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -6,6 +8,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_money_formatter/flutter_money_formatter.dart';
 
 import '../Providers/listing_provider.dart';
+import '../Screens/listing_screen.dart';
+import '../Widgets/listing_screen_arguments.dart';
 
 class ListingItem extends StatelessWidget {
   final String id;
@@ -39,198 +43,203 @@ class ListingItem extends StatelessWidget {
 
     FlutterMoneyFormatter fmf =
         FlutterMoneyFormatter(amount: double.parse('$price'));
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
-      ),
-      elevation: 5,
-      child: Padding(
-        padding: EdgeInsets.all(2),
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                FittedBox(
-                  child: Image.asset('$imagePath'),
-                  fit: BoxFit.fill,
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Spacer(),
-                          Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Color.fromRGBO(36, 119, 255, 0.6)),
-                                color: Color.fromRGBO(36, 119, 255, 0.6),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(100))),
-                            margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                            child: Row(
-                              children: <Widget>[
-                                FaIcon(
-                                  FontAwesomeIcons.binoculars,
-                                  color: Colors.white,
-                                  size: 12,
-                                ),
-                                Text(
-                                  ' $views',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Spacer(),
-                          Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Color.fromRGBO(36, 119, 255, 0.6)),
-                                color: Color.fromRGBO(36, 119, 255, 0.6),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(100))),
-                            margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                            child: Row(
-                              children: <Widget>[
-                                FaIcon(
-                                  FontAwesomeIcons.tags,
-                                  color: Colors.white,
-                                  size: 12,
-                                ),
-                                Text(
-                                  ' $offers',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Card(
-              child: Column(
+    return GestureDetector(
+      onTap: () { log('TAP');  Navigator.of(context).pushNamed(ListingScreen.routeName, arguments: ListingScreenArguments(id));} ,
+          child: Card(
+        
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(2),
+          child: Column(
+            children: <Widget>[
+              Stack(
                 children: <Widget>[
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Text(
-                        '$heading',
-                        style: GoogleFonts.lato(
-                          // posible heading style. will move to theme
-                          textStyle: TextStyle(
-                            letterSpacing: .25,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-
-                        // TextStyle(
-                        //   fontSize: 20,
-                        //   fontWeight: FontWeight.w600
-                        //  ),
-                      ),
-                    ),
+                  FittedBox(
+                    child: Image.asset('$imagePath'),
+                    fit: BoxFit.fill,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text('$streetNumber $streetName $city'),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            '${fmf.output.symbolOnLeft}',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                  ),
-                  Row(
-                    
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Color.fromRGBO(12, 82, 148, 0.2)),
-                            color: Color.fromRGBO(12, 82, 148, 0.2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100))),
-                        margin: EdgeInsets.all(5),
-                        padding: EdgeInsets.all(5),
-                        child: Row(
+                    alignment: Alignment.centerRight,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
                           children: <Widget>[
-                            FaIcon(FontAwesomeIcons.bed),
-                            Text(
-                              ' $bed',
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.black),
+                            Spacer(),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Color.fromRGBO(36, 119, 255, 0.6)),
+                                  color: Color.fromRGBO(36, 119, 255, 0.6),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(100))),
+                              margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                              child: Row(
+                                children: <Widget>[
+                                  FaIcon(
+                                    FontAwesomeIcons.binoculars,
+                                    color: Colors.white,
+                                    size: 12,
+                                  ),
+                                  Text(
+                                    ' $views',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Color.fromRGBO(12, 82, 148, 0.2)),
-                            color: Color.fromRGBO(12, 82, 148, 0.2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100))),
-                        margin: EdgeInsets.all(5),
-                        padding: EdgeInsets.all(5),
-                        child: Row(
+                        Row(
                           children: <Widget>[
-                            FaIcon(FontAwesomeIcons.bath),
-                            Text(
-                              ' $bath',
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.black),
+                            Spacer(),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Color.fromRGBO(36, 119, 255, 0.6)),
+                                  color: Color.fromRGBO(36, 119, 255, 0.6),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(100))),
+                              margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                              child: Row(
+                                children: <Widget>[
+                                  FaIcon(
+                                    FontAwesomeIcons.tags,
+                                    color: Colors.white,
+                                    size: 12,
+                                  ),
+                                  Text(
+                                    ' $offers',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                      Spacer(),
-                      IconButton(
-                        icon: FaIcon(FontAwesomeIcons.star),
-                        onPressed: () {},
-                        color: Colors.yellow,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
+              SizedBox(
+                height: 5,
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          '$heading',
+                          style: GoogleFonts.lato(
+                            // posible heading style. will move to theme
+                            textStyle: TextStyle(
+                              letterSpacing: .25,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+
+                          // TextStyle(
+                          //   fontSize: 20,
+                          //   fontWeight: FontWeight.w600
+                          //  ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text('$streetNumber $streetName $city'),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              '${fmf.output.symbolOnLeft}',
+                              style: TextStyle(fontSize: 16, color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                    ),
+                    Row(
+                      
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Color.fromRGBO(12, 82, 148, 0.2)),
+                              color: Color.fromRGBO(12, 82, 148, 0.2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100))),
+                          margin: EdgeInsets.all(5),
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: <Widget>[
+                              FaIcon(FontAwesomeIcons.bed),
+                              Text(
+                                ' $bed',
+                                style:
+                                    TextStyle(fontSize: 12, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Color.fromRGBO(12, 82, 148, 0.2)),
+                              color: Color.fromRGBO(12, 82, 148, 0.2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100))),
+                          margin: EdgeInsets.all(5),
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: <Widget>[
+                              FaIcon(FontAwesomeIcons.bath),
+                              Text(
+                                ' $bath',
+                                style:
+                                    TextStyle(fontSize: 12, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          icon: FaIcon(FontAwesomeIcons.star),
+                          onPressed: () {},
+                          color: Colors.yellow,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
