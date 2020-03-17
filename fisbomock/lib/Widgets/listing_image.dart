0897d6ac5235940c8d4becc70e591851b10bff65
FloydWatson <floydbcseit@gmail.com>
2020-03-17 13:08:33 +1300

    basic image swipe

--- fisbomock/lib/Widgets/listing_image.dart
+++ fisbomock/lib/Widgets/listing_image.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -18,45 +20,46 @@ class ListingImage extends StatelessWidget {
         padding: EdgeInsets.all(2),
         child: Column(
           children: <Widget>[
-            Stack(
-              children: <Widget>[
-                FittedBox(
-                  child: Image.asset('${listingProvider.activeUrl}'),
-                  fit: BoxFit.fill,
-                ),
-                Positioned.fill(
-                  child: Column(
-                    children: <Widget>[
-                      Spacer(),
-                      Row(
-                        children: <Widget>[
-                          IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.arrowCircleLeft,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              listingProvider.getNewPhotoLeft(id);
-                            },
-                          ),
-                          Spacer(),
-                          IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.arrowCircleRight,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              listingProvider.getNewPhotoRight(id);
-                            },
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                    ],
+             Stack(
+                children: <Widget>[
+                  FittedBox(
+                    child: GestureDetector(child: Image.asset('${listingProvider.activeUrl}'),  onHorizontalDragEnd: (DragEndDetails details) {log('${details.primaryVelocity} ${details.velocity}'); details.primaryVelocity > 0 ? listingProvider.getNewPhotoLeft(id) : listingProvider.getNewPhotoRight(id); },),
+                    fit: BoxFit.fill,
                   ),
-                ),
-              ],
-            ),
+                  Positioned.fill(
+                    child: Column(
+                      children: <Widget>[
+                        Spacer(),
+                        Row(
+                          children: <Widget>[
+                            IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.arrowCircleLeft,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                listingProvider.getNewPhotoLeft(id);
+                              },
+                            ),
+                            Spacer(),
+                            IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.arrowCircleRight,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                listingProvider.getNewPhotoRight(id);
+                              },
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            
           ],
         ),
       ),
